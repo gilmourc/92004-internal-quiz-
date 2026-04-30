44f580013d501506fa9efd3c653c470b9f4ef539
gilmourc <gilmourc@stu.otc.school.nz>
2026-05-01 09:57:46 +1200

    I added questions

--- internalquiz.py
+++ internalquiz.py
@@ -1,1 +1,11 @@
-'''This program will quiz the user on a New Zealand film'''
+'''This program will quiz the user on the New Zealand TV show, Shortland Street'''
+#This stores the questions and answers that the user will be asked
+questions = {"When did Shortland Street debut?(DD/MM/YYYY" : "25/05/1992",
+            "True or false. Is Shortland Street the longest running NZ TV show?" : "True",
+            "What year did Ed Sheeran have a cameo on the show?" : "2014",
+            "What drink replaced sparkling wine with when filming?" : "L&P",
+            "How many episodes are produced in 5, 10-hour working days?" : "5",
+            "Who was the first gay character in the show?(Answer with the character's name)" : "Jamie Forest",
+            "Who is the longest running original character?" : "Dr. Chris Warner"
+            "How many original characters are there? A) 13, B) 14, C) 15, D) 16" : "D"
+            }
