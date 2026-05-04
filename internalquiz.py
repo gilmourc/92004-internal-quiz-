50c550efb1fa72f58c2d3036a231be3c224fd0b2
gilmourc <gilmourc@stu.otc.school.nz>
2026-05-04 13:23:37 +1200

    vchgvuyfgcyfc

--- internalquiz.py
+++ internalquiz.py
@@ -1,11 +1,25 @@
 '''This program will quiz the user on the New Zealand TV show, Shortland Street'''
+
 #This stores the questions and answers that the user will be asked
-questions = {"When did Shortland Street debut?(DD/MM/YYYY" : "25/05/1992",
+questions = {"When did Shortland Street debut?(DD/MM/YYYY)" : "25/05/1992",
             "True or false. Is Shortland Street the longest running NZ TV show?" : "True",
             "What year did Ed Sheeran have a cameo on the show?" : "2014",
             "What drink replaced sparkling wine with when filming?" : "L&P",
             "How many episodes are produced in 5, 10-hour working days?" : "5",
-            "Who was the first gay character in the show?(Answer with the character's name)" : "Jamie Forest",
-            "Who is the longest running original character?" : "Dr. Chris Warner"
+            "Who was the first gay character in the show?(Answer with the character's stage name)" : "Jamie Forest",
+            "Who is the longest running original character?" : "Dr. Chris Warner",
             "How many original characters are there? A) 13, B) 14, C) 15, D) 16" : "D"
             }
+
+#Keeps track of the user's score
+score = 0
+
+#Using a for loop will provide the user with questions
+for questions, answers in questions:
+    user_answer = input(questions)
+    if user_answer.lower() == answers.lower():
+        print("Correct!")
+        score += 1
+    else:
+        try:
+            user_answer.lower() == answers.lower()
