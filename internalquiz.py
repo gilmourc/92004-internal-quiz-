feec283c387e30035b2f6104cdb7c2f07a61b10d
gilmourc <gilmourc@stu.otc.school.nz>
2026-05-11 13:27:47 +1200

    I tried adding stuff.. didn't work

--- internalquiz.py
+++ internalquiz.py
@@ -4,15 +4,18 @@
 questions = {"True or false. Did Shortland street debut in 1995?" : False,
             "True or false. Is Shortland Street the longest running NZ TV show?" : True,
             "What year did Ed Sheeran have a cameo on the show?" : 2014,
-            "What drink replaced sparkling wine with when filming?" : "L&P",
+            "What drink replaced sparkling wine with when filming?\nA) L&P,\nB) Sprite,\nC) Ginger Beer,\nD) None\n" : "A",
             "How many episodes are produced in 5, 10-hour working days?" : 5,
             "Who was the first gay character in the show?(Answer with the character's stage name)" : "Jamie Forest",
             "Who is the longest running original character?" : "Dr. Chris Warner",
-            "How many original characters are there? A) 13, B) 14, C) 15, D) 16" : "D"
+            "How many original characters are there?\nA) 13,\nB) 14,\nC) 15,\nD) 16\n" : "D"
             }
 
 #Keeps track of the user's score
 score = 0
+
+#asks the user for the answer to the questions
+user_answer = input(question)
 
 #Using a for loop will provide the user with questions
 for question, answer in questions.items():
@@ -41,20 +44,14 @@ for question, answer in questions.items():
                     print("False")
                     user_answer == True
             except ValueError:
-                user_answer = False
                 print("Please re-enter a valid number.")
                 user_answer = int(input())
-                
+                user_answer = False
+    if type(answer) == bool:
 
 
 
 
 
-
-
-
-
-
-percent = score/10
-
-        
+percent = round((score/len(question))*1000,2)
+print(f"{percent}%")
