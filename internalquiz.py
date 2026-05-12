1780994a2e43a207e0e7893d3521ac5f9033d68c
gilmourc <gilmourc@stu.otc.school.nz>
2026-05-12 12:00:52 +1200

    I worked on the string input stuff because it wasnt working andddddd it still isnt and I made it even worse than before

--- internalquiz.py
+++ internalquiz.py
@@ -19,6 +19,9 @@ questions = {"True or false. Did Shortland street debut in 1995?" : "False",
 #Keeps track of the user's score
 score = 0
 
+truefalse = ["true", "false"]
+multichoice = ["a", "b", "c", "d"]
+
 #Using a for loop will provide the user with questions
 for question, answer in questions.items():
     while True:
@@ -26,29 +29,55 @@ for question, answer in questions.items():
         user_answer = input(question)
         if type(answer) == str:
             try:
-                if answer.lower() == user_answer.lower():
-                    print("Correct! Next question!")
-                    score += 1
-                else:
-                    print("Wrong. Moving on.")
-                break
-            except Exception as error:
-                print(error)
+                if answer.lower() in truefalse:
+                    if user_answer.lower() == answer.lower():
+                        print("Correct. Next question")
+                        score += 1
+                        break
+                    else:
+                        print("Wrong. Next question.")
+                        break
+            except ZeroDivisionError: 
+                print("Please enter either true or false.")
+            try:
+                if answer.lower() == multichoice:
+                    if user_answer.lower() in multichoice:
+                        print("Correct! Next question.")
+                        score += 1
+                        break
+                    else:
+                        print("Wrong. Next question.")
+                        break
+            except TypeError:
+                print("Please answer from the list of mulitchoice questions")
+
         elif type(answer) == int:
             try:
-                user_answer == int(user_answer)
+                user_answer = int(user_answer)
                 if user_answer == answer:
                     print("Correct! Next question")
                     score += 1
-                elif user_answer <= 0:
+                    break
+                elif int(user_answer) <= 0:
                     print("Please enter a number larger than 0.")
-                elif user_answer > 2026:
+                elif int(user_answer) > 2026:
                     print("Please enter a number less than 2026.")
                 else:
                     print("False. Moving on.")
-                break
+                    break
             except ValueError:
                 print("Please enter a valid number!")
 
 percent = round((score/len(questions))*100,2)
 print(f"{percent}%")
+
+if percent == 100:
+    print("yay perfect score")
+elif percent >= 80:
+    print("good job")
+elif percent >= 60:
+    print(" you did ok")
+elif percent >= 40:
+    print("at least you tried")
+else:
+    print("better luck next time")
