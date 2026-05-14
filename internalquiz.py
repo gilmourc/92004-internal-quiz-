0f4dee5d2282580e457535bead937c0877c7712b
gilmourc <gilmourc@stu.otc.school.nz>
2026-05-15 08:59:11 +1200

    I fixed the multichoice questions and made them crash proof

--- internalquiz.py
+++ internalquiz.py
@@ -9,65 +9,69 @@ print("Have fun!")
 questions = {"True or false. Did Shortland street debut in 1995?" : "False",
             "True or false. Is Shortland Street the longest running NZ TV show?" : "True",
             "What year did Ed Sheeran have a cameo on the show?" : 2014,
-            "What drink replaced sparkling wine with when filming?\nA) L&P,\nB) Sprite,\nC) Ginger Beer,\nD) None\n" : "A",
+            "What drink replaced sparkling wine with when filming?\n1) L&P,\n2) Sprite,\n3) Ginger Beer,\n4) None" : 1,
             "How many episodes are produced in 5, 10-hour working days?" : 5,
             "True or false. Is Jamie Forest the first homosexual character in the show?" : "True",
             "True or false. Was Dr. Chris Warner recently introduced to the show" : "False",
-            "How many original characters are there?\nA) 13,\nB) 14,\nC) 15,\nD) 16\n" : "D"
+            "How many original characters are there?\n1) 13,\n2) 14,\n3) 15,\n4) 16" : 4
             }
 
 #Keeps track of the user's score
 score = 0
 
-truefalse = ["true", "false"]
-multichoice = ["a", "b", "c", "d"]
+#write somethign here
+multichoice = [1, 2, 3, 4]
 
 #Using a for loop will provide the user with questions
 for question, answer in questions.items():
     while True:
-        #asks the user for the answer to the question
-        user_answer = input(question)
+        user_answer = input(f"{question}\n")
         if type(answer) == str:
             try:
-                if answer.lower() in truefalse:
-                    if user_answer.lower() == answer.lower():
-                        print("Correct. Next question")
-                        score += 1
-                        break
-                    else:
-                        print("Wrong. Next question.")
-                        break
-            except ZeroDivisionError: 
+                if user_answer.lower() == answer.lower():
+                    print("Correct. Next question")
+                    score += 1
+                    break
+                else:
+                    print("Wrong.")
+                    break
+            except TypeError: 
                 print("Please enter either true or false.")
+            
+        elif type(answer) == int:
             try:
-                if answer.lower() == multichoice:
-                    if user_answer.lower() in multichoice:
+                if answer in multichoice:
+                    if int(user_answer) == int(answer):
                         print("Correct! Next question.")
                         score += 1
                         break
+                    elif int(user_answer) <= 0:
+                        print("Please choose a number from 1-4")
+                    elif int(user_answer) >= 5:
+                        print("Please choose a number from 1-4")
                     else:
                         print("Wrong. Next question.")
                         break
-            except TypeError:
-                print("Please answer from the list of mulitchoice questions")
-
-        elif type(answer) == int:
-            try:
-                user_answer = int(user_answer)
-                if user_answer == answer:
-                    print("Correct! Next question")
-                    score += 1
-                    break
-                elif int(user_answer) <= 0:
-                    print("Please enter a number larger than 0.")
-                elif int(user_answer) > 2026:
-                    print("Please enter a number less than 2026.")
-                else:
-                    print("False. Moving on.")
-                    break
             except ValueError:
-                print("Please enter a valid number!")
+                print("Please answer from the number of mulitchoice questions")
 
+            try:
+                if answer not in multichoice:
+                    if int(user_answer) == int(answer):
+                        print("Correct! Next question")
+                        score += 1
+                        break
+                    elif int(user_answer) <= 0:
+                        print("Please enter a number larger than 0.")
+                    elif int(user_answer) > 2026:
+                        print("Please enter a number less than 2026.")
+                    else:
+                        print("False. Moving on.")
+                        break
+            except ValueError:
+                print("Please answer a valid number")
+
+   
 percent = round((score/len(questions))*100,2)
 print(f"{percent}%")
 
